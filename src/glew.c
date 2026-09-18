f0beaa4bedcefb9bc8d09d78a25382366ec40e96
Gyusun Yeom(Automated) <omniavinco+auto@gmail.com>
2026-09-18 01:57:44 +0000

    Generate Sources of master updated at Fri Sep 18 01:57:44 GMT 2026

--- src/glew.c
+++ src/glew.c
@@ -127,7 +127,7 @@ void* NSGLGetProcAddress (const GLubyte *name)
   addr = dlsym(image, (const char*)name);
   if( addr ) return addr;
 #ifdef GLEW_APPLE_GLX
-  return dlGetProcAddress( name ); // try next for glx symbols
+  return dlGetProcAddress( name ); /* try next for glx symbols */
 #else
   return NULL;
 #endif
@@ -160,7 +160,7 @@ void* NSGLGetProcAddress (const GLubyte *name)
   free(symbolName);
   if( symbol ) return NSAddressOfSymbol(symbol);
 #ifdef GLEW_APPLE_GLX
-  return dlGetProcAddress( name ); // try next for glx symbols
+  return dlGetProcAddress( name ); /* try next for glx symbols */
 #else
   return NULL;
 #endif
